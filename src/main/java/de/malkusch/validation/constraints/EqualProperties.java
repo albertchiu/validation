1cc7be45b4a45e1db2bcf4c0a30196771a2e554c
Markus Malkusch <markus@malkusch.de>
2013-12-14 05:31:51 +0700

    documentation

--- src/main/java/de/malkusch/validation/constraints/EqualProperties.java
+++ src/main/java/de/malkusch/validation/constraints/EqualProperties.java
@@ -20,7 +20,8 @@ import de.malkusch.validation.validator.internal.EqualPropertiesValidator;
  * Add this constraint at class level and name the property names
  * which have to be equal. Set {@link #violationOnPropery()} to true
  * if you want the {@link ConstraintViolation} on each of the violating
- * properties instead of the bean.
+ * properties instead of the bean. Properties which have Null values
+ * don't violate against this constraint.
  * 
  * @author Markus Malkusch <markus@malkusch.de>
  * @since 0.1.0
@@ -38,7 +39,7 @@ public @interface EqualProperties {
 	Class<? extends Payload>[] payload() default {};
 
 	/**
-	 * @return property names which have to have equal values
+	 * @return property names (at least two) which have to have equal values.
 	 */
 	String[] value();
 	
