077b61dcffed30a3fadca574cfdf2b5eb40ee723
Markus Malkusch <markus@malkusch.de>
2014-03-16 11:32:54 +0700

    bitcoin validation for length and first byte

--- src/main/java/de/malkusch/validation/validator/internal/banking/BitcoinAddressValidatorForString.java
+++ src/main/java/de/malkusch/validation/validator/internal/banking/BitcoinAddressValidatorForString.java
@@ -3,6 +3,9 @@ package de.malkusch.validation.validator.internal.banking;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import com.google.bitcoin.core.AddressFormatException;
+import com.google.bitcoin.core.Base58;
+
 import de.malkusch.validation.constraints.banking.BitcoinAddress;
 
 public class BitcoinAddressValidatorForString implements ConstraintValidator<BitcoinAddress, String> {
@@ -18,7 +21,17 @@ public class BitcoinAddressValidatorForString implements ConstraintValidator<Bit
 				return true;
 				
 			}
+			
+			// Simple validation for length, characters and the first byte.
+			boolean match = address.matches("^[13][a-zA-Z1-9]{26,34}$");
+			if (! match) {
+				return false;
+				
+			}
+			
+			// Base58 decoding and validation of checksum.
 			Base58.decodeChecked(address);
+			
 			return true;
 			
 		} catch (AddressFormatException e) {
